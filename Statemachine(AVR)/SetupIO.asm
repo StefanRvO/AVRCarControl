570aaed318a8a91644723eb1463e10859532459b
StefanRvo <stefan@stefanrvo.dk>
2014-03-10 17:10:30 +0100

    reverted back to stack reset style. Made some additions and actual motor control

--- Statemachine(AVR)/SetupIO.asm
+++ Statemachine(AVR)/SetupIO.asm
@@ -4,3 +4,9 @@ out     DDRB,R16
 ;turn On half the diodes
 ldi     R16,0b01010101
 out     PORTB,R16
+;Set MOTOR pin as output
+SBI     DDRD,7
+ldi     R19,0       ;Turn motor off
+out     OCR2,R19
+ldi     R19,0x61
+out     TCCR2,R19   ;Phase corrected PWM, no prescale
--- Statemachine(AVR)/main.asm
+++ Statemachine(AVR)/main.asm
@@ -1,5 +1,4 @@
-;R21,R22,R23 is state registers, R21 holds recived data, R22 holds state (Automode, etc) and R23 holds mode , Get,Set.. etc. Memory adress 0x200-0x202 holds former states
-;VERY buggy
+
 .include "m32def.inc"
 
 .org    0x0000
@@ -7,12 +6,8 @@
 .org    0x0060
 Reset:
 .include "SetupStack.asm"       ;setup the stack
-.include "SetupSerial.asm"      ;setup serial connection
+.include "SetupSerial16Mhz.asm"      ;setup serial connection
 .include "SetupIO.asm"
-ldi R23,0xFF
-ldi R22,0xFF
-ldi R21,0xFF
-
 
     sei                         ;enable interrupts
     jmp     Main
@@ -22,209 +17,126 @@ ldi R21,0xFF
 ;****
 ;******************* RECIVE INTERRUPT
 RX_ISR: ;We always recive 3 bytes, put them in R16:R18
-    ;Push  to stack
-    push    R16
-    push    R17
-    push    R18
-    in      R16,SREG
-    push    R16
-    in      R16,UDR ;read recived into R16
-    ;wait for new byte
-    RX_WAIT1:
-    SBIS    UCSRA,RXC
-    RJMP    RX_WAIT1
-    in  R17,UDR     ;Read into R17
-    RX_WAIT2:
-    SBIS    UCSRA,RXC
-    RJMP    RX_WAIT2
-    in    R18,UDR   ;Read into R18
-    CALL StateSet
-    pop     R18
-    out     SREG,R18
-    pop     R18
-    pop     R17
-    pop     R16
+push    R16 ;Push  to stack
+push    R17
+push    R18
+in      R16,UDR ;read recived into R16
+;wait for new byte
+RX_WAIT1:
+SBIS    UCSRA,RXC
+RJMP    RX_WAIT1
+in  R17,UDR     ;Read into R17
+RX_WAIT2:
+SBIS    UCSRA,RXC
+RJMP    RX_WAIT2
+in    R18,UDR   ;Read into R18
+CALL STATESET
+pop     R18
+pop     R17
+pop     R16
 RETI
-StateSet:
-    ;Set the states in Reg35-33
-    sts 0x200,R23
-    sts 0x201,R22
-    sts 0x202,R21
-    
-    cpi R16,0x55    ;Set mode?
-    BRNE Checkget
-    ldi R23,0x00
-    mov R22,r17
-    mov R21,r18
-    ret
-    Checkget:
-    
-    cpi R16,0xaa    ;Get mode?
-    brne endcheck
-    ldi R23,0x01
-    mov R22,R17
-    mov R21,R18
-    endcheck:
-    ret
-    
-    
-    
 
-TRANSREPLY:  ;Sends the data in R17:R19
-    SBIS    UCSRA,UDRE
-    RJMP    TRANSREPLY
-    out     UDR,R17
-    TRANSREPLY1:
-    SBIS    UCSRA,UDRE
-    RJMP    TRANSREPLY1
-    out     UDR,R18
-    TRANSREPLY2:
-    SBIS    UCSRA,UDRE
-    RJMP    TRANSREPLY2
-    out     UDR,R19
+
+;****************************CHANGE STATE
+STATESET:
+CPI R16,0x55
+BRNE    PC+2
+CALL     SET
+STATESET1:
+CPI R16,0xAA
+BRNE    PC+2
+CALL     GET
+STATERET:
+RET
+;******************
+
+;*************Set mode
+SET:	
+;call specific loop
+CPI R17,0x10
+BRNE    PC+2
+CALL SETSPEED
+CPI R17,0x11
+BRNE    PC+2
+CALL STOP
+CPI R17,0x12
+BRNE    PC+2
+CALL AUTOMODE
+RET
+;*********
+;****************GETMODE
+GET:
+push    R20        
+push    R21
+push    R22
+ldi     R20,0xBB
+;****                   ;HERE WE SHOULD
+ldi     R21,0x00        ;FETCH THE WANTED 
+in      R22,OCR2       ;DATA
+;****                   ;
+CALL    TRANSREPLY
+pop     R20
+pop     R21
+pop     R22
+RET
+;******************
+
+TRANSREPLY:  ;Sends the data in R20:R22
+SBIS    UCSRA,UDRE
+RJMP    TRANSREPLY
+out     UDR,R20
+TRANSREPLY1:
+SBIS    UCSRA,UDRE
+RJMP    TRANSREPLY1
+out     UDR,R21
+TRANSREPLY2:
+SBIS    UCSRA,UDRE
+RJMP    TRANSREPLY2
+out     UDR,R22
 RET
 ;***************
 
 SETSPEED:
-    cpi R23,0x00
-    breq    PC+2
-    ret
-    SETSPEED2:
-    cpi R22,0x10
-    breq    PC+2
-    ret
-    SETSPEED3:
-    nop
-    out PORTB,R21
-rjmp SETSPEED  ; SET motor speed dependent on R18 value
+;GET OUT OF INTERRUPT MODE, clear the stack 
+		ldi		R16, HIGH(RAMEND)       ;THIS IS UGLY
+		out		SPH, R16                ;ANOTHER
+		ldi		R16, LOW(RAMEND)        ;SOLUTION
+		out		SPL, R16                ;IS NEEDED!!
+	    sei		
+;*********
+nop ;convert from 0-100 to 0-255
+out OCR2,R18
+jmp Main ; SET motor speed dependent on R18 value
 
 STOP:
-    cpi R23,0x00
-    breq    PC+2
-    ret
-    cpi R22,0x11
-    breq    PC+2
-    ret
-    ldi R21,0x00
-    out PORTB,R21
-ret
-
-AutoMode:
-    cpi R23,0x00
-    breq    PC+2
-    ret
-    cpi R22,0x12
-    breq    PC+2
-    ret
-    ldi R21,0xee   
-    out PORTB,R18
-rjmp    AutoMode
-
-SET:
-    cpi R23,0x00
-    breq SET1
-    ret
-    SET1:
-    cpi R22,0x10
-    brne SET2
-    call SETSPEED
-    SET2:
-    cpi R22,0x11
-    brne SET3
-    call STOP
-    SET3:
-    cpi R22,0x12
-    brne SET4
-    call AutoMode
-    SET4:
-rjmp SET
-
-Get:
-    cpi R23,0x01
-    breq GETCHECK1
-    ret
-    GETCHECK1:
-    cpi  R22,0x10 ;Get speed data
-    brne    GETCHECK2
-    call GETSPEED
-    GETCHECK2:
-    cpi R22,0x11 
-    brne    GETCHECK3
-    call GETSTOP     ;Get stop data
-    GETCHECK3:
-    cpi R22,0x12
-    brne    ENDGETCHECK    ;Get Automode data
-    call GETAUTOMODE
-    ;Return to last state 
-    ENDGETCHECK:   
-    lds R23,0x200
-    lds R22,0x201
-    lds R21,0x202
-    rjmp Get
-
-GETSPEED:
-push    R17
-push    R18
-push    R19
-ldi     R17,0xbb
-ldi     R18,0x10
-in      R19,PORTB   ;Read value from port B. This should be changed to actual motor speed
-CALL    TRANSREPLY
-pop     R19
-pop     R18
-pop     R17
-RET
-
-GETSTOP:
-push    R17
-push    R18
-push    R19
-push    R20
-ldi     R17,0xbb
-ldi     R18,0x11
-in      R20,PORTB   ;Read value from port B. This should be changed to actual motor speed
-ldi     R19,0x00
-cpi     R20,0x00
-brne    PC+2
-ldi     R19,0x01
-CALL    TRANSREPLY
-pop     R20
-pop     R19
-pop     R18
-pop     R17
-RET
-
-GETAUTOMODE:
-push    R17
-push    R18
-push    R19
-push    R20
-ldi     R17,0xbb
-ldi     R18,0x12
-lds     R20,0x201
-ldi     R19,0x00
-cpi     R20,0x12
-brne    PC+2
-ldi     R19,0x01
-CALL    TRANSREPLY
-pop     R20
-pop     R19
-pop     R18
-pop     R17
-RET
+;GET OUT OF INTERRUPT MODE, clear the stack 
+		ldi		R16, HIGH(RAMEND)       ;THIS IS UGLY
+		out		SPH, R16                ;ANOTHER
+		ldi		R16, LOW(RAMEND)        ;SOLUTION
+		out		SPL, R16                ;IS NEEDED!!
+	    sei		
+;*********
+ldi R18,0x00
+out OCR2,R18
+jmp Main
+AUTOMODE:
+;GET OUT OF INTERRUPT MODE, clear the stack 
+		ldi		R16, HIGH(RAMEND)       ;THIS IS UGLY
+		out		SPH, R16                ;ANOTHER
+		ldi		R16, LOW(RAMEND)        ;SOLUTION
+		out		SPL, R16                ;IS NEEDED!!
+	    sei		
+;*********
+ldi R18,0x00
+AutoModeLoop:
+dec R18   ;Do some artimitic, this is just a placeholder
+out OCR2,R18
+jmp AutoModeLoop
 ;******
 ;*MAIN
 ;******
-
 Main:
-    cpi R23,0x00
-    brne PC+2
-    Call    SET
-    cpi R23,0x01
-    brne PC+2
-    call    Get
 RJMP    Main
 
 
 
-;;RECODE TO WHILE LOOPS...!! CPI, BREQ +2, RET..??
